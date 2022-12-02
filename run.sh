b574e3e5d411e002cad3d43943c0e5a3d935bff5
wangyuchi369 <wangyuchi369@163.com>
2022-12-02 16:27:55 +0800

    add case study

---
+++ run.sh
@@ -0,0 +1,20 @@
+python run.py \
+--model_name_or_path hfl/chinese-roberta-wwm-ext \
+--do_train \
+--do_eval \
+--train_file CHID_dataset/train_data_1w.json \
+--validation_file CHID_dataset/dev_data.json \
+--test_file CHID_dataset/test_data.json \
+--metric_for_best_model eval_accuracy \
+--load_best_model_at_end \
+--learning_rate 5e-5 \
+--evaluation_strategy epoch \
+--num_train_epochs 5 \
+--output_dir ./tmp \
+--per_device_eval_batch_size 30 \
+--per_device_train_batch_size 8 \
+--seed 42 \
+--max_seq_length 512 \
+--warmup_ratio 0.1 \
+--save_strategy epoch \
+--overwrite_output
